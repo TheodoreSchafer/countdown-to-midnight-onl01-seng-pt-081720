50333ce1337bf5e29b330b6591efb3c3937d8aec
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-20 22:50:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 def countdown(num)
-while num > 0
-  puts "#{num} SECOND(S)!"
-  num -=1
-  end
+  while num > 0
+    puts "#{num} SECOND(S)!"
+      num -=1
+    end
   return "HAPPY NEW YEAR!"
 end
 
